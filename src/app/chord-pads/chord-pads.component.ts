0b815067e193c7ef8cdb2ad68a7e707cb37bc7c7
m4st3rm1ndtm <m4st3rm1nd.tm@gmail.com>
2022-07-14 12:36:57 +0300

    2 files modified, 2 added

---
+++ src/app/chord-pads/chord-pads.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chord-pads',
+  templateUrl: './chord-pads.component.html',
+  styleUrls: ['./chord-pads.component.css']
+})
+export class ChordPadsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
