6c52b04f54dcfba7a5b3b1b224091e968813e570
m4st3rm1ndtm <m4st3rm1nd.tm@gmail.com>
2022-07-14 13:21:50 +0300

    added chord tiles with dummy numbers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+import { MatGridListModule } from '@angular/material/grid-list';
+
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { ChordPadsComponent } from './chord-pads/chord-pads.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, MatGridListModule],
   declarations: [AppComponent, HelloComponent, ChordPadsComponent],
   bootstrap: [AppComponent],
 })
--- src/app/chord-pads/chord-pads.component.ts
+++ src/app/chord-pads/chord-pads.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-chord-pads',
   templateUrl: './chord-pads.component.html',
-  styleUrls: ['./chord-pads.component.css']
+  styleUrls: ['./chord-pads.component.css'],
 })
 export class ChordPadsComponent implements OnInit {
+  chordRows: number[][][] = [
+    [
+      [10, 11, 12],
+      [13, 14, 15],
+    ],
+    [
+      [16, 17, 18],
+      [19, 20, 21],
+    ],
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
